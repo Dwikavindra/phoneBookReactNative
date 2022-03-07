922dc2409a74dd59865fff7ca3ca78e8e50b07a2
Dwikavindra Haryo Radithya <dwika@Dwikavindras-MacBook-Air.local>
2022-03-07 22:09:02 +0700

    Revert back to previous non-seperate Modal Add Contact

--- App.tsx
+++ App.tsx
@@ -8,10 +8,9 @@
  * @format
  */
 
-import React from 'react';
-import {useState} from 'react';
+import * as React from 'react';
 import styles from './styles';
-import {BackHandler, SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 import {
   Searchbar,
   TextInput,
@@ -20,35 +19,29 @@ import {
   Modal,
   Portal,
 } from 'react-native-paper';
-import ModalAddContact from './ModalAddContact';
 
 export function App() {
-  const [text, setText] = useState<string>(' ');
-  const [visible, setVisible] = useState(false);
-  const showModal = () => setVisible(true);
-  const hideModal = () => setVisible(false);
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Text style={styles.title}>Phone Book</Text>
+<<<<<<< HEAD
         <ModalAddContact
           visible={visible}
           onDismiss={hideModal}
           setVisible={setVisible}></ModalAddContact>
+=======
+
+>>>>>>> parent of 94a03dd (added Modal)
         <View style={styles.textInput}>
           <TextInput
-            onChange={() => {
-              setText(text);
-            }}
             style={styles.textInputPadding}
-            value={text}
+            value="Search by phone number or name"
             mode="flat"
             left={<TextInput.Icon name="magnify"></TextInput.Icon>}></TextInput>
         </View>
       </View>
-
       <IconButton
-        onPress={showModal}
         style={styles.floatingActionButton}
         icon="plus"
         size={50}
