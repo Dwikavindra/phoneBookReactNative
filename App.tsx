491859fd468f128c187fa88a10ade4dcbe3e46d3
Dwikavindra Haryo Radithya <dwika@Dwikavindras-MacBook-Air.local>
2022-03-07 22:36:08 +0700

    Switch modal to react-native modal due to bugs in react paper

--- App.tsx
+++ App.tsx
@@ -1,47 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import * as React from 'react';
+import React, {useEffect} from 'react';
+import {useState} from 'react';
 import styles from './styles';
-import {SafeAreaView, Text, View} from 'react-native';
-import {
-  Searchbar,
-  TextInput,
-  IconButton,
-  Colors,
-  Modal,
-  Portal,
-} from 'react-native-paper';
+
+import {BackHandler, SafeAreaView, Text, View} from 'react-native';
+import {Searchbar, TextInput, IconButton, Colors} from 'react-native-paper';
+import ModalAddContact from './ModalAddContact';
 
 export function App() {
+  const [text, setText] = useState<string>();
+  const [visible, setVisible] = useState(false);
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const onChangeTextName = (text: string) => setText(text);
+  const containerStyle = {backgroundColor: 'white', padding: 20};
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Text style={styles.title}>Phone Book</Text>
-<<<<<<< HEAD
         <ModalAddContact
           visible={visible}
-          onDismiss={hideModal}
-          setVisible={setVisible}></ModalAddContact>
-=======
-
->>>>>>> parent of 94a03dd (added Modal)
+          onDismiss={hideModal}></ModalAddContact>
         <View style={styles.textInput}>
           <TextInput
+            onChangeText={text => onChangeTextName(text)}
             style={styles.textInputPadding}
-            value="Search by phone number or name"
-            mode="flat"
-            left={<TextInput.Icon name="magnify"></TextInput.Icon>}></TextInput>
+            selectionColor="blue"
+            value={text}
+            mode="outlined"
+            left={<TextInput.Icon name="magnify"></TextInput.Icon>}
+            activeOutlineColor={Colors.blue100}
+            theme={{colors: {background: 'transparent'}}}></TextInput>
         </View>
       </View>
+
       <IconButton
+        onPress={showModal}
         style={styles.floatingActionButton}
         icon="plus"
         size={50}
--- ModalAddContact.tsx
+++ ModalAddContact.tsx
@@ -1,30 +1,19 @@
-import {Modal, Portal} from 'react-native-paper';
+import Modal from 'react-native-modal';
 import styles from './styles';
 import {Text, View} from 'react-native';
 import React from 'react';
-import {useEffect, useState} from 'react';
 
 interface ModalAddContactProps {
   visible: boolean;
   onDismiss: () => void;
-  setVisible: Function;
 }
 
 export default function ModalAddContact(props: ModalAddContactProps) {
-  const [visible, setVisible] = useState(props.visible);
-
-  useEffect(() => {
-    () => {
-      props.setVisible(props.visible);
-    };
-  }, []);
   return (
-    <Portal>
-      <Modal visible={props.visible} onDismiss={() => props.onDismiss()}>
-        <View style={styles.modalAddContact}>
-          <Text>Hello World</Text>
-        </View>
-      </Modal>
-    </Portal>
+    <Modal isVisible={props.visible} onBackdropPress={props.onDismiss}>
+      <View style={styles.modalAddContact}>
+        <Text>Hello!</Text>
+      </View>
+    </Modal>
   );
 }
