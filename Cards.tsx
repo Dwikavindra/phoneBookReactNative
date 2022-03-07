e340b38e3ac10c9e5d429e1abbdb9a1549a4d694
Dwikavindra Haryo Radithya <dwika@Dwikavindras-MacBook-Air.local>
2022-03-08 03:59:59 +0700

    Added CardsView

--- Cards.tsx
+++ Cards.tsx
@@ -2,6 +2,8 @@ import {SafeAreaView, Text, View, FlatList} from 'react-native';
 import {GlobalProvider} from './context/GlobalState';
 import React, {useState, useContext, useEffect} from 'react';
 import {GlobalContext, ContactStates} from './context/GlobalState';
+import {Avatar, Card, IconButton} from 'react-native-paper';
+
 const Cards = () => {
   const {contacts} = useContext(GlobalContext) as ContactStates;
 
@@ -9,7 +11,15 @@ const Cards = () => {
     <View style={{flex: 1, marginTop: 50, alignItems: 'center'}}>
       {contacts.map((value, index) => {
         console.log(contacts);
-        return <Text>{value.name}</Text>;
+        return (
+          <Card.Title
+            title={value.name}
+            subtitle={value.phoneNumber}
+            left={() => (
+              <Avatar.Image size={50} source={{uri: value.image_uri}} />
+            )}
+          />
+        );
       })}
     </View>
   );
