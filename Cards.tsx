45c4f1356ccf1b411c8d978e1b623567921bf68e
Dwikavindra Haryo Radithya <dwika@Dwikavindras-MacBook-Air.local>
2022-03-08 04:14:19 +0700

    Added CardsView With Subtitle and Avatar Icon

--- Cards.tsx
+++ Cards.tsx
@@ -1,26 +1,29 @@
-import {SafeAreaView, Text, View, FlatList} from 'react-native';
+import {SafeAreaView, Text, View, FlatList, ScrollView} from 'react-native';
 import {GlobalProvider} from './context/GlobalState';
 import React, {useState, useContext, useEffect} from 'react';
 import {GlobalContext, ContactStates} from './context/GlobalState';
-import {Avatar, Card, IconButton} from 'react-native-paper';
+import {Avatar, Card, Colors, IconButton} from 'react-native-paper';
 
 const Cards = () => {
   const {contacts} = useContext(GlobalContext) as ContactStates;
 
   return (
-    <View style={{flex: 1, marginTop: 50, alignItems: 'center'}}>
-      {contacts.map((value, index) => {
-        console.log(contacts);
-        return (
-          <Card.Title
-            title={value.name}
-            subtitle={value.phoneNumber}
-            left={() => (
-              <Avatar.Image size={50} source={{uri: value.image_uri}} />
-            )}
-          />
-        );
-      })}
+    <View style={{flex: 1, marginTop: 5}}>
+      <ScrollView style={{flex: 1, marginTop: 60}}>
+        {contacts.map((value, index) => {
+          console.log(contacts);
+          return (
+            <Card.Title
+              style={{flex: 4}}
+              title={value.name}
+              subtitle={value.phoneNumber}
+              left={() => (
+                <Avatar.Image size={50} source={{uri: value.image_uri}} />
+              )}
+            />
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
